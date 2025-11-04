f33415d760d7b0d6577f98fa7a2cec2096fa19aa
w-junizar <junijar.w@gmail.com>
2025-11-04 12:20:24 +0700

    Add requirements.txt for dependencies

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+import redis
+import os
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
-def home():
-    greeting = 'Hello World!'
-    return render_template('index.html', greeting=greeting)
+def hello():
+    count = cache.incr('hits')
+    return f'Hello World! I have been seen {count} times.\n'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
